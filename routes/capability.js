bee4e51161560bb2b2883b8cd201e62aa729df86
Erwandi Pramudya <epramudya@aio.co.id>
2024-04-18 16:54:49 +0700

    finishing

--- routes/capability.js
+++ routes/capability.js
@@ -1,12 +1,14 @@
 var express = require('express');
 const { 
     getMstJenisPengujian,
-    countCapability
+    countCapability,
+    dataTable
 } = require('../controllers/capabilityController');
 var router = express.Router();
 
 
 router.get('/capability-al4-mst-jenispengujian', getMstJenisPengujian);
 router.post('/count-capability', countCapability);
+router.post('/data-table', dataTable);
 
 module.exports = router;
