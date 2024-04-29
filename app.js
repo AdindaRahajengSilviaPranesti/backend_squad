e0bbadb992bd74549293b086123102a98efd33f3
Erwandi Pramudya <epramudya@aio.co.id>
2024-04-29 14:02:58 +0700

    before the day

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors())
-
+// console.log('baru');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
